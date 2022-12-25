47b37f0ac6ca85fd801c9a5a480c76dbe27242f8
DastinDV <komander_72@rambler.ru>
2022-12-25 21:07:26 +0300

    finished

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import requests
+from send_email import send_email
+
+topic = "tesla"
 
 api = "9e88c356fcd74aa6a3f9262ef6d31c58"
-url = "https://newsapi.org/v2/everything?q=tesla" \
+url = f"https://newsapi.org/v2/everything?q={topic}" \
       "&from=2022-11-25&sortBy=publishedAt&apiKey=" \
-      "9e88c356fcd74aa6a3f9262ef6d31c58"
+      "9e88c356fcd74aa6a3f9262ef6d31c58"\
+      "&language=ru"
 
 request = requests.get(url)
-content = request.json() 
+content = request.json()
 
-for article in content["articles"]:
-      print(article["title"])
-      print(article["description"])
+message = ""
+
+for article in content["articles"][:10]:
+    if article["title"] is not None:
+        message += f"""\
+Subject: Today's news
+Title: {article["title"]}
+Description: {article["description"]}
+{article["url"]}
+""" + "\n"
+
+message = message.encode("utf-8")
+send_email(message)
